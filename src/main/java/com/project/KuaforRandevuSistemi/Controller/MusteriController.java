352c63586c08dec15060ed60c5df8322308ce8cc
Beyza Arslan <beyzarslan865@gmail.com>
2025-05-25 00:32:49 +0300

    controller guncellendi

--- src/main/java/com/project/KuaforRandevuSistemi/Controller/MusteriController.java
+++ src/main/java/com/project/KuaforRandevuSistemi/Controller/MusteriController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/musteri")
 public class MusteriController {
